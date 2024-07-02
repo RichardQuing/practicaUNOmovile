bb09c19ae48b8de774e262cfe567b47d3b2853a7
verozambrano <vrnc098@gmail.com>
2024-07-02 09:49:36 -0500

    gracias

---
+++ hola.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function hola() {
+  return (
+    <View>
+      <Text>hola</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
